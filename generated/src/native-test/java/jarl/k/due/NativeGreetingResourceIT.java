298438b024d1967b68dbd7ef799845bffb1907f4
Jarl <jkd@miracle.dk>
2021-01-27 20:56:27 +0100

    Adding generated to gitignore

--- generated/src/native-test/java/jarl/k/due/NativeGreetingResourceIT.java
+++
@@ -1,9 +0,0 @@
-package jarl.k.due;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeGreetingResourceIT extends GreetingResourceTest {
-
-    // Execute the same tests but in native mode.
-}
